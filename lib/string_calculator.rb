0f11d72b3d7fe30b230e0b87711edea21a7c6500
Roderick Nuque <enuke4@gmail.com>
2022-02-07 16:44:16 +0000

    learning rspec

---
+++ lib/string_calculator.rb
@@ -0,0 +1,13 @@
+class StringCalculator
+
+  def self.add(input)
+    if input.empty?
+      0
+    else
+      numbers = input.split(",").map { |num|num.to_i } 
+      numbers.inject(0) { |sum, number| sum+number }
+      # input.to_i
+    end
+  end
+  
+end
